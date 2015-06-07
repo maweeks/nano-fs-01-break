90d4b2d8daa52afc94e4047f7600793432ddfcf4
Matt Weeks <m_weeks@hotmail.co.uk>
2015-06-08 00:43:40 +0100

    Get list of files.

--- 02-rename.py
+++ 02-rename.py
@@ -1,10 +1,10 @@
-import time
-import webbrowser
-count = 0
-start_time = time.ctime()
-print ("This program started at "+start_time)
-while (count < 8):
-	time.sleep(3600)
-	webbrowser.open("http://matt.weeks.codes")
-	count += 1
-	print ("Take a break, the time is now,  "+start_time)
+import os
+
+def rename_files():
+	#get file names
+	file_list = os.listdir(r"/Users/MattBP/Documents/Nano-FS/lessons/nano-fs-01-lesson/prank")
+	print(file_list)
+
+	#rename files
+
+rename_files()
