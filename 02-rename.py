2ed4385121ceaa82ac4f61bc8cf930b8c2b810ee
Matt Weeks <m_weeks@hotmail.co.uk>
2015-06-08 00:53:38 +0100

    Fixed.

--- 02-rename.py
+++ 02-rename.py
@@ -2,9 +2,12 @@ import os
 
 def rename_files():
 	#get file names
-	file_list = os.listdir(r"/Users/MattBP/Documents/Nano-FS/lessons/nano-fs-01-lesson/prank")
+	source = "/Users/MattBP/Documents/Nano-FS/lessons/nano-fs-01-lesson/prank/"
+	file_list = os.listdir(r"/Users/MattBP/Documents/Nano-FS/lessons/nano-fs-01-lesson/prank/")
 	print(file_list)
 
 	#rename files
+	for file_name in file_list:
+		os.rename((source+file_name), (source+file_name.translate(None, "0123456789")))
 
 rename_files()
