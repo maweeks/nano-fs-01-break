f2bae690a96e088b21450e3b8e25dc36e4d24ccd
Matt Weeks <m_weeks@hotmail.co.uk>
2015-06-08 00:21:08 +0100

    Show time of each break.

--- break.py
+++ break.py
@@ -1,8 +1,10 @@
 import time
 import webbrowser
 count = 0
+start_time = time.ctime()
+print ("This program started at "+start_time)
 while (count < 8):
-	time.sleep(1)
-	print count
-	#webbrowser.open("http://matt.weeks.codes")
+	time.sleep(3600)
+	webbrowser.open("http://matt.weeks.codes")
 	count += 1
+	print ("Take a break, the time is now,  "+start_time)
