94d47438cbc4a6ee1290544291426cb6d8b45148
Matt Weeks <m_weeks@hotmail.co.uk>
2015-06-08 01:48:37 +0100

    Draw circleSquare.

--- 03-drawing.py
+++ 03-drawing.py
@@ -1,53 +1,65 @@
 import turtle
 
-def draw_circle():
-	larry = turtle.Turtle()
-	larry.shape("turtle")
-	larry.color("red")
-	#larry.speed(2)
-	larry.circle(50)
+def draw_circle(turts):
+	turts.circle(50)
 
-def draw_pentagon():
-	jimmy = turtle.Turtle()
-	jimmy.shape("turtle")
-	jimmy.color("yellow")
-	#larry.speed(2)
+def draw_pentagon(turts):
 	x=0
 	while (x<5):
-		jimmy.forward(100)
-		jimmy.right(72)
+		turts.forward(100)
+		turts.right(72)
 		x+=1
 
-def draw_square():
-	barry = turtle.Turtle()
-	barry.shape("turtle")
-	barry.color("green")
-	#larry.speed(2)
+def draw_square(turts):
 	x=0
 	while (x<4):
-		barry.forward(100)
-		barry.right(90)
+		turts.forward(100)
+		turts.right(90)
 		x+=1
 
-def draw_triangle():
-	harry = turtle.Turtle()
-	harry.shape("turtle")
-	harry.color("white")
-	#larry.speed(2)
+def draw_triangle(turts):
 	x=0
 	while (x<3):
-		harry.forward(100)
-		harry.right(120)
+		turts.forward(100)
+		turts.right(120)
+		x+=1
+
+def draw_circle_square(turts):
+	x=0
+	while (x<36):
+		draw_square(turts)
+		turts.right(10)
 		x+=1
 
 def draw_stuff():
 	window = turtle.Screen()
 	window.bgcolor("blue")
 
-	draw_circle()
-	draw_pentagon()
-	draw_square()
-	draw_triangle()
+	larry = turtle.Turtle()
+	larry.shape("turtle")
+	larry.color("red")
+	#larry.speed(2)
+	draw_circle(larry)
+
+	jimmy = turtle.Turtle()
+	jimmy.shape("turtle")
+	jimmy.color("yellow")
+	draw_pentagon(jimmy)
+
+	barry = turtle.Turtle()
+	barry.shape("turtle")
+	barry.color("green")
+	draw_square(barry)
+
+	harry = turtle.Turtle()
+	harry.shape("turtle")
+	harry.color("white")
+	draw_triangle(harry)
+
+	bobby = turtle.Turtle()
+	bobby.shape("turtle")
+	bobby.color("pink")
+	draw_circle_square(bobby)
 
 	window.exitonclick()
 
